05024c3250ecb786e9b236d0b11f78bbe190d730
Axoford12 <postmaster@axoford12.cc>
2017-02-08 22:01:53 +0800

    Add index.

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -1,20 +1,43 @@
 <?php
+use yii\helpers\Url;
 
-/* @var $this yii\web\View */
-
-$this->title = 'Axoford12\'s blog';
 ?>
-<div class="site-index">
+<div class="col-lg-8">
+    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <?php foreach ($data as $k => $list): ?>
+                <li data-target="#carousel-example-generic" data-slide-to="<?= $k ?>" class="active"></li>
+            <?php endforeach; ?>
+        </ol>
 
-    <div class="jumbotron">
-        <h1>Axo还在努力开荒哦</h1>
-    </div>
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner home-banner" role="listbox">
+            <?php foreach ($data as $k => $list): ?>
+                <div class="item <?=$k == 0?'active':''?>">
+                    <a href="<?= Url::to(['post/index'],['id' => $list['id']]) ?>"><img class="img-responsive" src="<?= $list['label_img'] != '0'
+                            ? $list['label_img']
+                            : 'http://p5.img.cctvpic.com/nettv/ent/program/xin'
+                            .'xiyouji/20110727/images/100529_5464_1311924643253.jpg'?>" alt="Axo">
+                        <div class="carousel-caption">
 
-    <div class="body-content">
-
-        <div class="row">
+                        </div>
+                    </a>
+                </div>
+            <?php endforeach; ?>
 
         </div>
 
+        <!-- Controls -->
+        <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left"></span>
+        </a>
+        <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right"></span>
+        </a>
     </div>
+    <?=\common\widgets\ArticleList\ArticleList::widget()?>
+</div>
+<div class="col-lg-3">
+
 </div>
